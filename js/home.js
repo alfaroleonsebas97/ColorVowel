593c4cc2d88d9e0264cde23b4d5749d576687790
Sebastian Alfaro <alfaroleonsebas97@gmail.com>
2022-03-17 01:09:28 -0600

    Cambiar idioma, ajustar header.

--- js/home.js
+++ js/home.js
@@ -1,13 +1,27 @@
-'use strict';
-
+"use strict";
 
 function populateSelect() {
-    var dropdown = document.getElementById('subjects');
-    for (var i = 0; i < subjects.length; i++) {
-        // POPULATE SELECT ELEMENT WITH JSON.
-        dropdown.innerHTML = dropdown.innerHTML +
-            '<option value="' + subjects[i]['ID'] + '">' + subjects[i]['Subject_Name'] + '</option>';
-    }
+  var dropdown = document.getElementById("subjects");
+  for (var i = 0; i < subjects.length; i++) {
+    // POPULATE SELECT ELEMENT WITH JSON.
+    dropdown.innerHTML =
+      dropdown.innerHTML +
+      '<option value="' +
+      subjects[i]["ID"] +
+      '">' +
+      subjects[i]["Subject_Name"] +
+      "</option>";
+  }
 }
 
-populateSelect();
+function main() {
+  populateSelect();
+
+  $('[lang="es"]').hide();  
+  $(".switch-lang").click(function () {
+    $('[lang="es"]').toggle();
+    $('[lang="en"]').toggle();
+  });
+}
+
+window.addEventListener("load", main);
