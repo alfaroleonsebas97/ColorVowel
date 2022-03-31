a382e3d86ab3da4b8456b49ed12a897148a829dc
Susy Mora <susymmora@gmail.com>
2022-03-31 15:08:03 -0600

    Primer grupo de imagenes

--- js/subjects.js
+++ js/subjects.js
@@ -1,19 +1,19 @@
 var subjects = [
 
-    {"ID": "003", "Subject_Name": "Physical Appearance"},
-    {"ID": "003", "Subject_Name": "Feelings"},
+    {"ID": "001", "Subject_Name": "Physical Appearance"},
+    {"ID": "002", "Subject_Name": "Feelings"},
     {"ID": "003", "Subject_Name": "Cardinal numbers"},
-    {"ID": "003", "Subject_Name": "Colors"},
+    {"ID": "004", "Subject_Name": "Colors"},
 
-    {"ID": "003", "Subject_Name": "School supplies"},
-    {"ID": "002", "Subject_Name": "Classroom"},
+    {"ID": "005", "Subject_Name": "School supplies"},
+    {"ID": "006", "Subject_Name": "Classroom"},
 
-    {"ID": "001", "Subject_Name": "Family"},
-    {"ID": "003", "Subject_Name": "House"},
+    {"ID": "007", "Subject_Name": "Family"},
+    {"ID": "008", "Subject_Name": "House"},
 
-    {"ID": "003", "Subject_Name": "Action Verbs"},
-    {"ID": "003", "Subject_Name": "Hobbies"},
+    {"ID": "009", "Subject_Name": "Action Verbs"},
+    {"ID": "010", "Subject_Name": "Hobbies"},
 
-    {"ID": "003", "Subject_Name": "Professions"},
-    {"ID": "003", "Subject_Name": "Days of the week"},
+    {"ID": "011", "Subject_Name": "Professions"},
+    {"ID": "012", "Subject_Name": "Days of the week"},
   ];
