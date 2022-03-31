454b465566cff966acadf2f6c267a39e3a2a474b
Susy Mora <susymmora@gmail.com>
2022-03-31 14:36:31 -0600

    Subjects

--- js/subjects.js
+++ js/subjects.js
@@ -1,5 +1,19 @@
 var subjects = [
-    {"ID": "001", "Subject_Name": "Family"},
+
+    {"ID": "003", "Subject_Name": "Physical Appearance"},
+    {"ID": "003", "Subject_Name": "Feelings"},
+    {"ID": "003", "Subject_Name": "Cardinal numbers"},
+    {"ID": "003", "Subject_Name": "Colors"},
+
+    {"ID": "003", "Subject_Name": "School supplies"},
     {"ID": "002", "Subject_Name": "Classroom"},
+
+    {"ID": "001", "Subject_Name": "Family"},
     {"ID": "003", "Subject_Name": "House"},
+
+    {"ID": "003", "Subject_Name": "Action Verbs"},
+    {"ID": "003", "Subject_Name": "Hobbies"},
+
+    {"ID": "003", "Subject_Name": "Professions"},
+    {"ID": "003", "Subject_Name": "Days of the week"},
   ];
